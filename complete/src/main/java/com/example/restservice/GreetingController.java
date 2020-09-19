68613fb92a43b37967efb4f3cb2222d433babd0c
Vishal <Vishal.Karki@gmail.com>
2020-09-19 14:02:14 +0530

    Added a new class

--- complete/src/main/java/com/example/restservice/GreetingController.java
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GreetingController {
+@RequestMapping("/Hello")
+public class GreetingController1 {
 
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
@@ -16,12 +17,24 @@ public class GreetingController {
 	public String sessions() {
 		return "{\"message\":\"Hello Worldv1\"}";;
 	}
-	
+
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "User") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 
+}
 
+@RestController
+public class GreetingController2 {
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "User") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
 
 }
