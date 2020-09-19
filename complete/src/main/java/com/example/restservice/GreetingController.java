1a5518f2e2e7438055ac9dfcc3a2aa9bf32c0397
Vishal <Vishal.Karki@gmail.com>
2020-09-19 12:52:09 +0530

    Added a new class

--- complete/src/main/java/com/example/restservice/GreetingController.java
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -13,7 +13,13 @@ public class GreetingController {
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "User") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
+
+	@GetMapping("/session")
+	public String sessions() {
+		return Sessions.GetSession();
+	}
+
 }
